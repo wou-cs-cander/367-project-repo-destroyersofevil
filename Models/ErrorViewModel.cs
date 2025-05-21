ed39447f8f55ac688c9e407eac14d419191dd8a6
Erin Anderson <eranderson14@mail.wou.edu>
2025-05-21 11:24:25 -0700

    Corrected shared file
    There was a double shared file and I fixed it

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _367_project_repo_destroyersofevil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
