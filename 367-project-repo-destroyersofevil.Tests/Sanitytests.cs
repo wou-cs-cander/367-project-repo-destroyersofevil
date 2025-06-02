d4ab20456e58fc5798e47387ddd0e8494e694242
Philip Staniunas <pstaniunas23@mail.wou.edu>
2025-06-02 15:20:50 -0700

    Its kind of working, but its annoying

---
+++ 367-project-repo-destroyersofevil.Tests/Sanitytests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _367_project_repo_destroyersofevil.Tests
+{
+    [TestClass]
+    public class SanityTests
+    {
+        [TestMethod]
+        public void TrueIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ 367-project-repo-destroyersofevil.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace _367_project_repo_destroyersofevil.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
