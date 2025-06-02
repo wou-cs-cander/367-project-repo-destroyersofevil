c684b81417492c3d072d44c0cd351a4f3b329e33
Philip Staniunas <pstaniunas23@mail.wou.edu>
2025-06-01 19:09:37 -0700

    Singled out where im experiencing errors, created sanitytests

---
+++ 367_project_repo_destroyersofevil.Tests/SanityTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _367_project_repo_destroyersofevil.Tests.Controllers
+{
+    [TestClass]
+    public class SanityTest
+    {
+        [TestMethod]
+        public void TestFrameworkIsWorking()
+        {
+            Assert.AreEqual(1, 1); // Should always pass
+        }
+    }
+}
